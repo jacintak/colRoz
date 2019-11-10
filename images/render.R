9b749ddac56f5bb6c574c8cb3a357408464a7b90
jacintak <48912454+jacintak@users.noreply.github.com>
2019-11-10 21:08:57 +0000

    added render palette script

--- images/render.R
+++ images/render.R
@@ -1,9 +1,12 @@
+# A script to render all palettes as png into the images folder
+
 for (i in 1:length(oz_palettes)){
   for (j in 1:length(oz_palettes[[i]])){
+    if(!is.null(names(oz_palettes[[i]]))){
     png(units = "px", width = 400, height = 250, res = 120,
-         filename = paste0("images\\",
-                           names(oz_palettes[[i]])[j], ".png"))
+         filename = paste0("images/",names(oz_palettes[[i]])[j], ".png"))
     print_palette(colRoz_pal(names(oz_palettes[[i]])[j]))
     dev.off()
+    }
   }
 }
