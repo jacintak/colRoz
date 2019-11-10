d00ee725bf4e74f0aef7c66db1ea50cff149aedf
Nicholas Wu <nicholas.wu.nz@gmail.com>
2019-11-10 22:36:32 +1100

    Update oz_palettes.R
    - edited oz_palettes to warramba_palettes
    - added lizard_palette
    - grouped warramba_palettes and lizard_palettes together

--- R/oz_palettes.R
+++ R/oz_palettes.R
@@ -12,7 +12,9 @@
 #' @keywords colours
 #' @export
 #'
-oz_palettes <- list(
+oz_palettes <- do.call(c, list(warramba_palettes, lizard_palettes))
+
+warramba_palettes <- list(
   grandis = rbind(c("#911B1B", "#985775","#BAAE94","#DE7184","#74892E","#FF9A6E"),c(1,2,3,4,5,6)),
   flavolineata = rbind(c("#573333","#E0C5BE","#FDD41A", "#D3AD64", "#7A5D55","#DE5C00"),c(1,2,3,4,5,6)),
   whitei = rbind(c("#E5A430", "#9C7210","#D7A8B8", "#BAB24F", "#392821", "#9B391B"),c(1,2,3,4,5,6)),
@@ -21,3 +23,13 @@ oz_palettes <- list(
   ngadju = rbind(c("#3D2E27", "#BF6938", "#F3B65D", "#9D9C42", "#EBDECE", "#664D51"),c(1,2,3,4,5,6))
 )
 
+lizard_palettes <- list(
+  m.horridus = rbind(c("#6F2305", "#D29155", "#975428", "#477298", "#224263"),c(1,2,3,4,5)),
+  t.nigrolutea = rbind(c("#FFDAA0", "#B29952", "#A38847", "#F9A338", "#867673", "#0F120F"),c(1,2,3,4,5,6)),
+  e.leuraensis = rbind(c("#e2b745", "#e3bb88","#988e42", "#4e3d1d", "#1b1702"),c(1,2,3,4,5)),
+  c.decresii = rbind(c("#DCA761","#C6C16D", "#8B9C94", "#628CA5", "#5A6C7A", "#514F5C"),c(1,2,3,4,5,6))
+)
+
+# landscape_palettes # to do
+# bird_palettes # to do
+# frog_palettes # to do
