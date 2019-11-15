75ffb6f8f931d51a6da1cfe6c4043794f4d94916
Nicholas Wu <nicholas.wu.nz@gmail.com>
2019-11-16 09:26:19 +1100

    added requested palettes

--- R/oz_palettes.R
+++ R/oz_palettes.R
@@ -44,22 +44,35 @@ oz_palettes <- list(
     s.spinigerus = rbind(c("#18181a","#848178","#c6c7c2", "#815b37", "#e3c77c","#feff99"),c(1,2,3,4,5,6))
   ),
   landscapes = list(
-    uluru = rbind(c("#543813","#E4733B", "#D9842A", "#DAB77F", "#CFE0D8", "#9CC3E0", "#5981BC"),c(1,2,3,4,5,6,7)),
-    shark_bay = rbind(c( "#11100b","#313534", "#353922","#274f68", "#89b8c0", "#e6ecea"),c(1,2,3,4,5,6)),
-    sky = rbind(c( "#9dbfda","#6fa3dd", "#588ece","#336aaa", "#295a9c","#1b4287"),c(1,2,3,4,5,6)),
-    desert_sunset = rbind(c( "#626063", "#ad5e59","#fd7c45","#faba62","#ffffbb", "#bbdadc"),c(1,2,3,4,5,6)),
-    desert_dusk = rbind(c( "#040605", "#0a183f","#1c407a","#a1a4b7","#f2bfa4", "#ffd585"),c(1,2,3,4,5,6)),
-    desert_flood = rbind(c( "#552c10", "#865b48","#343611","#515530","#977f7d", "#d3b696"),c(1,2,3,4,5,6)),
-    salt_lake = rbind(c( "#1b396d", "#3e88b9","#34362b","#b6bfbe","#5d3f27", "#c7b29d"),c(1,2,3,4,5,6)),
-    daintree = rbind(c( "#020401","#0e1203","#101e07","#2a4613","#374e0a", "#7d8b3c"),c(1,2,3,4,5,6)),
-    spinifex = rbind(c( "#10100e","#697573","#384a22", "#8d8b4e","#d6aa85", "#e4e6d1"),c(1,2,3,4,5,6))
+    uluru = rbind(c("#543813","#E4733B","#D9842A","#DAB77F","#CFE0D8","#9CC3E0","#5981BC"), c(1,2,3,4,5,6,7)),
+    shark_bay = rbind(c( "#11100b","#313534", "#353922","#274f68","#89b8c0","#e6ecea"), c(1,2,3,4,5,6)),
+    sky = rbind(c( "#9dbfda","#6fa3dd", "#588ece","#336aaa","#295a9c","#1b4287"), c(1,2,3,4,5,6)),
+    desert_sunset = rbind(c( "#626063", "#ad5e59","#fd7c45","#faba62","#ffffbb","#bbdadc"), c(1,2,3,4,5,6)),
+    desert_dusk = rbind(c("#040605", "#0a183f","#1c407a","#a1a4b7","#f2bfa4","#ffd585"), c(1,2,3,4,5,6)),
+    desert_flood = rbind(c("#552c10", "#865b48","#343611","#515530","#977f7d","#d3b696"), c(1,2,3,4,5,6)),
+    salt_lake = rbind(c("#1b396d", "#3e88b9","#34362b","#b6bfbe","#5d3f27","#c7b29d"), c(1,2,3,4,5,6)),
+    daintree = rbind(c("#020401","#0e1203","#101e07","#2a4613","#374e0a","#7d8b3c"), c(1,2,3,4,5,6)),
+    spinifex = rbind(c("#10100e","#697573","#384a22", "#8d8b4e","#d6aa85","#e4e6d1"), c(1,2,3,4,5,6)),
+    nq.stream = rbind(c("#808314","#DCDB43","#C5DFF4","#323526","#534C2E"),c(1,2,3,4,5))
   ),
   birds = list(
-    p.cincta = rbind(c("#CFC7C4","#75767A","#DCA074","#994B1E","#593C2A","#1B1110"), c(1,2,3,4,5,6))
+    p.cincta = rbind(c("#CFC7C4","#75767A","#DCA074","#994B1E","#593C2A","#1B1110"), c(1,2,3,4,5,6)),
+    c.azureus = rbind(c("#2E3780","#384AB3","#7C444D","#A34D36","#B4AFAF","#272A28"), c(1,2,3,4,5,6)),
+    m.cyaneus = rbind(c("#659FCB","#05090F","#477173","#22372C","#3A6C4E"), c(1,2,3,4,5))
   ),
   frogs = list(),
   snakes = list(
     a.ramsayi = rbind(c("#17170b", "#422811","#5a3111","#a06322","#e9c74a" ,"#f5eacc"),c(1,2,3,4,5,6))
+  ),
+  plant = list(
+    n.violacea = rbind(c("#301E48","#796D9D","#DDDAEF","#F9E011","#D9B523","#946D2A"), c(1,2,3,4,5,6)),
+    xantho = rbind(c("#736F66","#1D1C1A","#2E3F13","#90A53E","#E1AF4C","#A36407"), c(1,2,3,4,5,6))
+  ),
+  fish = list(
+    r.aculeatus = rbind(c("#7B6E18","#24566D","#617764","#9DD2CB","#B3C29A","#BBCC64"), c(1,2,3,4,5,6))
+  ),
+  inverts = list(
+    p.mitchelli = rbind(c("#3F2723","#F4F4DA","#9B7B66","#DBF379","#4F6A35"), c(1,2,3,4,5))
   )
 )
 
